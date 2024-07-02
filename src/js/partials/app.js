0832e95e36508f494a67ca56238a6d177f809e57
prixodik <prixodik@gmail.com>
2024-07-02 22:40:51 +0700

    three section

--- src/js/partials/app.js
+++ src/js/partials/app.js
@@ -5,6 +5,7 @@ var uikit = {
     sm: '992',
     xs: '640',
     xxs: '480',
+	controller: new ScrollMagic.Controller(),
     ww: function(){
         return $(window).width();
     },
@@ -17,7 +18,6 @@ var uikit = {
 		var opacity = 1;
 		var anim = setInterval(() => {
 			$('.js-dotted').each(function(){ 
-				//var $this = $(this);
 				opacity = !opacity;
 				$(this).css('transform', 'translate(' + Math.ceil(Math.random() * 70) + 'px, ' + Math.ceil(Math.random() * 70) + 'px)').css('opacity',  opacity);
 			});
@@ -25,9 +25,61 @@ var uikit = {
 		
 	},
 
+	animThree: function () {
+		//var scene = document.querySelector(".js-three-section");
+		var threeHeight = this.wh(); //1250;
+		var sun = document.getElementsByClassName('js-three-sun');
+		var city = document.getElementsByClassName('js-three-city');
+		var heroCenter = document.getElementsByClassName('js-three-hero-center');
+		var heroLeft = document.getElementsByClassName('js-three-hero-left');
+		var heroRight = document.getElementsByClassName('js-three-hero-right');
+
+		var tweenSun = new TimelineMax()
+			.to(sun, 1, {top: '-100px', transform: 'translateX(-50%) scale(1)'});
+		var tweenCity = new TimelineMax()
+			.to(city, 1, {top: '0%'});
+		
+		var tweenHeroCenter = new TimelineMax()
+			.to(heroCenter, 1, {top: '400px'});
+		var tweenHeroLeft = new TimelineMax()
+			.to(heroLeft, 1, {transform: 'translateY(0)'});
+		var tweenHeroRight = new TimelineMax()
+			.to(heroRight, 1, {transform: 'translateY(0)'});
+
+		var sceneThreeCity = new ScrollMagic.Scene({triggerElement: ".js-three-section", duration: (threeHeight * 1), offset: 0, triggerHook: 0.5})
+			//.setPin(".three-section__container")
+			.setTween(tweenCity)
+			.addIndicators({name: "three-city"})
+			.addTo(this.controller);
+
+		var sceneThreeSun = new ScrollMagic.Scene({triggerElement: ".js-three-section", duration: (threeHeight * 1), offset: 100, triggerHook: 0.5})
+			//.setPin(".js-three-section")
+			.setTween(tweenSun)
+			.addIndicators({name: "three-sun"})
+			.addTo(this.controller);
+
+		var sceneThreeHeroCenter = new ScrollMagic.Scene({triggerElement: ".js-three-section", duration: (threeHeight * 0.5), offset: 300, triggerHook: 0.8})
+			//.setPin(".js-three-section")
+			.setTween(tweenHeroCenter)
+			.addIndicators({name: "three-hero-center"})
+			.addTo(this.controller);
+
+		var sceneThreeHeroLeft = new ScrollMagic.Scene({triggerElement: ".js-three-section", duration: (threeHeight * 0.5), offset: 400, triggerHook: 0.8})
+			//.setPin(".js-three-section")
+			.setTween(tweenHeroLeft)
+			.addIndicators({name: "three-hero-left"})
+			.addTo(this.controller);
+
+		var sceneThreeHeroRight = new ScrollMagic.Scene({triggerElement: ".js-three-section", duration: (threeHeight * 0.5), offset: 400, triggerHook: 0.8})
+			//.setPin(".js-three-section")
+			.setTween(tweenHeroRight)
+			.addIndicators({name: "three-hero-right"})
+			.addTo(this.controller);
+	},
+
     mainInit: function () {
         this.dottedAnim();
-        
+        this.animThree();
     }
 };
 
@@ -59,7 +111,10 @@ $(document).ready(function() {
     });
 
     $(window).on('load', function() {
+		//Принудительная прокрутка вверх
+		$('html, body').animate({scrollTop: 0}, 'fast');
         // Все элементы страницы полностью загружены
+
         $('.js-loader').fadeOut(400);
 		$('.js-first-section').addClass('is-first-anim');
 		$('.js-two-section').addClass('is-first-anim');
@@ -67,7 +122,7 @@ $(document).ready(function() {
 		setTimeout(()=>{
 			$('.js-first-section').removeClass('is-first-anim');
 			$('.js-two-section').removeClass('is-first-anim');
-			$('body').removeClass('is-overflow');
+			$('body, html').removeClass('is-overflow');
 		}, 4000);
     });
 });
--- src/js/script.js
+++ src/js/script.js
@@ -1,6 +1,7 @@
 /* components */
 //=include ../../bower_components/jquery/dist/jquery.min.js
 
+//=include ./plugins/0TweenMax.min.js
 //=include ./plugins/ScrollMagic.min.js
 //=include ./plugins/jquery.ScrollMagic.min.js
 //=include ./plugins/animation.gsap.min.js
